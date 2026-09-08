47d1c66bf5bc00e73bbf80206c560b12a8fb870e
israel228912 <israelmartinsdossantos0007@gmail.com>
2026-09-08 19:48:02 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,8 @@
 function mostrar(id){
   var el = document.getElementById(id);
-  if(el.style.display === "block"){
-    el.style.display = "none";
-  } else {
-    el.style.display = "block";
-  }
+  if(el.style.display === "block"){ el.style.display = "none"; }
+  else{ el.style.display = "block"; }
 }
-
 function motivacao(){
   var frase = document.getElementById("frase");
   frase.innerHTML = "Não desista, você pode chegar nos seus objetivos! 💜";
